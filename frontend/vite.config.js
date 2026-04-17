47e74c2c797559586c499365e3edf8d43b1ef974
Shahzad Alam Bhatti <sbhatti@nisum.com>
2026-04-17 20:49:08 +0500

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),s],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     open: true,
